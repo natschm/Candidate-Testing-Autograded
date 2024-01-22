507e9f62ca7322f81909402e2f852dba815e03a6
natschm <costar-1@hotmail.com>
2024-01-22 00:18:20 -0600

    second set of tests passing

--- candidate-testing.js
+++ candidate-testing.js
@@ -10,11 +10,38 @@ let question = "Who was the first American woman in space? ";
 let correctAnswer = "Sally Ride";
 let candidateAnswer = ""; 
 
-//TODO: Variables for Part 2
-let questions;
-let correctAnswers;
-let candidateAnswers;
+// Question	Answer
+// Who was the first American woman in space?	"Sally Ride"
+// True or false: 5 kilometer == 5000 meters?	"true"
+// (5 + 3)/2 * 10 = ?	"40"
+// Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2?	"Trajectory"
+// What is the minimum crew size for the ISS?	3
 
+//TODO: Variables for Part 2
+
+// let questions;
+// let correctAnswers;
+// let candidateAnswers;
+
+let questions = ["Who was the first American woman in space? ",
+  "True or false: 5 kilometer == 5000 meters? ",
+  "(5 + 3)/2 * 10 = ? ",
+  "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ",
+  "What is the minimum crew size for the ISS? "
+ ];
+
+ let correctAnswers = ["Sally Ride","true",
+  "40",
+  "Trajectory",
+  "3"
+ ];
+
+ let candidateAnswers = [
+  "",
+  "",
+  "",
+  ""
+ ];
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
@@ -22,7 +49,7 @@ let candidateName = input.question("What is your name?");
 
 }
 
-function askQuestion() {
+function askQuestion() { 
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
   let candidateAnswer = input.question(question);
 
